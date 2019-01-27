552e51b190ae84535da0529e6fea10e7cec55a39
Anshul Ahuja <anshul.ahu@gmail.com>
2019-01-27 18:02:05 +0530

    Add Employer model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -8,3 +8,7 @@ class Freelancer(models.Model):
     # Foreign key to django's inbuilt user model which handles authentication and basic details related to a user like name, email etc
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+
+class Employer(models.Model):
+    # Foreign key to django's inbuilt user model which handles authentication and basic details related to a user like name, email etc
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
