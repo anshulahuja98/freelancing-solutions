9233cc917e604f48b66c46373779352e9502b1d8
Anshul Ahuja <anshul.ahu@gmail.com>
2019-01-27 18:02:28 +0530

    Add Bid model

--- src/jobs/models.py
+++ src/jobs/models.py
@@ -5,3 +5,10 @@ from accounts.models import Freelancer, Employer
 class Job(models.Model):
     employer = models.ForeignKey(Employer, null=True, on_delete=models.SET_NULL)
 
+
+class Bid(models.Model):
+    # Foreign key to a freelancer and employer
+    job = models.ForeignKey(Job, null=True, on_delete=models.SET_NULL)
+    freelancer = models.ForeignKey(Freelancer, null=True, on_delete=models.SET_NULL)
+    # Add tests for currency checks
+    bid_amount = models.DecimalField(max_digits=8, decimal_places=2)
