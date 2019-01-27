64d8b636bb3d88247e03d8c7121baab77b40f412
Anshul Ahuja <anshul.ahu@gmail.com>
2019-01-27 18:01:54 +0530

    Add Freelancer model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Userprofile models for different type of users
+
+class Freelancer(models.Model):
+    # Foreign key to django's inbuilt user model which handles authentication and basic details related to a user like name, email etc
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
