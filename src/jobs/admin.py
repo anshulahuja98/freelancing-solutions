a255b4781b83548168ad82dd582e7a9d30f0439f
Anshul Ahuja <anshul.ahu@gmail.com>
2019-01-28 18:23:52 +0530

    Register Skill on admin

--- src/jobs/admin.py
+++ src/jobs/admin.py
@@ -1,3 +1,10 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Skill, Job, Bid
 
-# Register your models here.
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Skill
+        fields = '__all__'
+
