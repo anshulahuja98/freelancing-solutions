bd173cd1caf1e4ba6c61f89b734029b1b270e7a7
Anshul Ahuja <anshul.ahu@gmail.com>
2019-01-28 13:32:34 +0530

    Register Freelancer on admin

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,3 +1,11 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Freelancer, Employer
 
 # Register your models here.
+
+@admin.register(Freelancer)
+class FreelancerAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Freelancer
+        fields = '__all__'
+
