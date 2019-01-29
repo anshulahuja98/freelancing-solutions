5f3532e2886de56be2bca6f330b7af3574681089
Chakshu Gupta <gupta.18@iitj.ac.in>
2019-01-29 23:07:03 +0530

    Refactor Userprofile models

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,15 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
 
-# Userprofile models for different type of users
+# Abstract User Profile model
+class AbstractUserProfile(models.Model):
+    # Regex Validators
+    mobile_num_valid = RegexValidator(r'^[0-9]{10}$', message='Not a valid mobile number!')
 
-class Freelancer(models.Model):
-    # Foreign key to django's inbuilt user model which handles authentication and basic details related to a user
+    # Fields
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    mobile = models.CharField(max_length=10, validators=[mobile_num_valid])
+    description = models.TextField()
+
+    class Meta:
+        abstract = True
+
+
+class Freelancer(AbstractUserProfile):
+    # Foreign key to django's inbuilt user model which handles authentication and basic details related to a user
     skills = models.ManyToManyField('jobs.Skill', default=None)
+    profile_image = models.ImageField(upload_to="freelancer-profile-images")
+
+    def __str__(self):
+        return self.user.get_full_name()
 
 
-class Employer(models.Model):
+class Employer(AbstractUserProfile):
     # Foreign key to django's inbuilt user model which handles authentication and basic details related to a user
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = models.ImageField(upload_to="employer-profile-images")
+
+    def __str__(self):
+        return self.user.get_full_name()
