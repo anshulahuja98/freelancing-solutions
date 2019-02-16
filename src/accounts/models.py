5bd016bc1ef92566f0d5cb73079120d1c1d91d21
Chakshu Gupta <chakshugupta2000@gmail.com>
2019-02-16 10:18:55 +0530

    Added field to AbstractUserProfile
    Updated mobile number validation regex
    Added fields: address & country

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
+from django_countries.fields import CountryField
 
 
 # Abstract User Profile model
 class AbstractUserProfile(models.Model):
     # Regex Validators
-    mobile_num_valid = RegexValidator(r'^[0-9]{10}$', message='Not a valid mobile number!')
-
+    mobile_num_valid = RegexValidator(r'^\+?1?\d{9,15}$', message='Invalid mobile number.')
     # Fields
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     mobile = models.CharField(max_length=10, validators=[mobile_num_valid])
     description = models.TextField()
+    address = models.TextField()
+    country = CountryField()
 
     class Meta:
         abstract = True
