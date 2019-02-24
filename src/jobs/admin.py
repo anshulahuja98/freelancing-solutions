db19bf969a5e6d4e2de71480ff0c281b597dab7a
Anshul Ahuja <anshul.ahu@gmail.com>
2019-02-24 11:59:39 +0530

    Add comments to jobs/admin

--- src/jobs/admin.py
+++ src/jobs/admin.py
@@ -1,23 +1,29 @@
 from django.contrib import admin
-from .models import Skill, Job, Bid
+from .models import Skill, Job, Bid  # Import Skill, Job, Bid module
 
 
+# Register Skill module on the admin interface
 @admin.register(Skill)
 class SkillAdmin(admin.ModelAdmin):
     class Meta:
         model = Skill
+        # All fields of Skill
         fields = '__all__'
 
 
+# Register Job module on the admin interface
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
     class Meta:
         model = Job
+        # All fields of Skill
         fields = '__all__'
 
 
+# Register Bid module on the admin interface
 @admin.register(Bid)
 class BidAdmin(admin.ModelAdmin):
     class Meta:
         model = Bid
+        # All fields of Skill
         fields = '__all__'
