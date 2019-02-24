63cc6e4432708407f2b41fb512b22b6b3e17538a
Anshul Ahuja <anshul.ahu@gmail.com>
2019-02-24 11:57:45 +0530

    Add comments to custom fields in jobs - rating and money field

--- src/jobs/fields.py
+++ src/jobs/fields.py
@@ -1,17 +1,22 @@
 from django.db.models import fields
 
 
+# Custom RatingField inherits from inbuilt DecimalField of django
 class MoneyField(fields.DecimalField):
-
+    # Override constructor of DecimalField of django
     def __init__(self, verbose_name=None, name=None, max_digits=8,
-                 decimal_places=2, **kwargs):
+                 decimal_places=2, **kwargs):  # Fix max digits to 8 and decimal places to 2
+        # Call constructor of the super class - DecimalField
         super().__init__(verbose_name, name, **kwargs)
+        # Set default max digits and decimal_places
         self.max_digits, self.decimal_places = max_digits, decimal_places
 
 
+# Custom RatingField inherits from inbuilt IntegerField of django
 class RatingField(fields.IntegerField):
-
+    # Override constructor of IntegerField of django
     def __init__(self, *args, **kwargs):
+        # Tuple of ratings presets
         RATINGS = (
             (0, 'NA'),
             (1, 'Very Poor'),
@@ -20,7 +25,7 @@ class RatingField(fields.IntegerField):
             (4, 'Good'),
             (5, 'Excellent'),
         )
-
+        # Sends the RATINGS in the keyword arg of choices
         kwargs['choices'] = RATINGS
-
+        # Call constructor of the super class - IntegerField
         super(RatingField, self).__init__(*args, **kwargs)
