29c3bf3d7177a900f44e760669ca3ba4021e36db
Geetesh Gupta <gupta.15@iitj.ac.in>
2019-02-27 19:16:28 +0530

    Create IndexView

--- src/main/urls.py
+++ src/main/urls.py
@@ -1,1 +1,6 @@
 from django.urls import path
+from .views import IndexView
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index')
+]
--- src/main/views.py
+++ src/main/views.py
@@ -1,1 +1,6 @@
 from django.views.generic import TemplateView
+
+
+# Create your views here.
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
