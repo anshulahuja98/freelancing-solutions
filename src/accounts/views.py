20422f5ad420d0fa5c90a36496b32e0e3a7a82fe
Anshul Ahuja <anshul.ahu@gmail.com>
2019-02-27 22:34:59 +0530

    Create Freelancer Registration view

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,4 +1,7 @@
 # from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import Freelancer, Employer
+from .forms import FreelancerRegisterForm, EmployerRegisterForm
 from django.contrib.auth.views import LoginView as DefaultLoginView
 
 
@@ -9,4 +12,28 @@ class LoginView(DefaultLoginView):
     def get_success_url(self):
         url = "dashboard/"
         return url
+
+
+class FreelancerRegisterFormView(CreateView):
+    model = Freelancer
+    form_class = FreelancerRegisterForm
+    template_name = 'accounts/freelancer-register.html'
+    success_url = '/accounts/login/'
+
+    def form_valid(self, form):
+        user = form.save()
+        self.create_profile(user, **form.cleaned_data)
+        return super(FreelancerRegisterFormView, self).form_valid(form)
+
+    @staticmethod
+    def create_profile(user=None, **kwargs):
+        # Creates a new UserProfile object after successful creation of User object
+        userprofile = Freelancer.objects.create(user=user,
+                                                mobile=kwargs['mobile'],
+                                                description=kwargs['description'],
+                                                address=kwargs['address'],
+                                                country=kwargs['country'],
+                                                # profile_image=kwargs['profile_image'],
+                                                )
+        userprofile.save()
 
