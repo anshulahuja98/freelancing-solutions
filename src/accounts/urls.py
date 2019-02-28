814c74e69c7233e87552d821ad3fb52ccee7cf3a
Geetesh Gupta <gupta.15@iitj.ac.in>
2019-02-28 09:05:48 +0530

    Add logout url in accounts

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import FreelancerRegisterFormView, EmployerRegisterFormView, LoginView
+from django.contrib.auth.views import LogoutView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='index'), name='logout'),
     path('register/freelancer/', FreelancerRegisterFormView.as_view(), name='freelancer-register'),
     path('register/employer/', EmployerRegisterFormView.as_view(), name='employer-register'),
 ]
