b7779f1a00bc385330b5383e8cb16792d96a30e6
Geetesh Gupta <gupta.15@iitj.ac.in>
2019-02-28 09:08:17 +0530

    Create LoginViewTest

--- src/accounts/tests.py
+++ src/accounts/tests.py
@@ -1,3 +1,14 @@
-# from django.test import TestCase
+from django.test import TestCase
 
-# Create your tests here.
+
+class LoginViewTest(TestCase):
+
+    def test_login_redirect(self):
+        response = self.client.post('/accounts/login')
+        self.assertRedirects(response, '/accounts/login/', status_code=301, target_status_code=200)
+
+    def test_login(self):
+        response = self.client.post('/accounts/login/')
+        self.assertTemplateUsed(response, 'accounts/login.html')
+
+
