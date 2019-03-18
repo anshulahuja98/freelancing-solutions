213db06139c374f349735dde6ca60924034e9433
Geetesh Gupta <gupta.15@iitj.ac.in>
2019-03-18 17:54:33 +0530

    Update logout url test

--- src/accounts/tests/test_views.py
+++ src/accounts/tests/test_views.py
@@ -7,7 +7,7 @@ class TestViews(TestCase):
     def setUp(self):
         self.client = Client()
         self.login_url = reverse('accounts:login')
-        self.logout_url = reverse('accounts:login')
+        self.logout_url = reverse('accounts:logout')
         self.freelancer_register_url = reverse('accounts:freelancer-register')
         self.employer_register_url = reverse('accounts:employer-register')
 
@@ -20,8 +20,8 @@ class TestViews(TestCase):
     def test_logout_GET(self):
         response = self.client.get(self.logout_url)
 
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'main/base.html')
+        self.assertEquals(response.status_code, 302)
+        self.assertRedirects(response, reverse('main:index'), 302, 200)
 
     def test_freelancer_register_GET(self):
         response = self.client.get(self.freelancer_register_url)
