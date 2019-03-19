dd69c6cacce5171fa26bcc34af686f55186af573
Geetesh Gupta <gupta.15@iitj.ac.in>
2019-03-19 09:57:52 +0530

    Update TestJobModel to test_get_average_bid

--- src/jobs/tests/test_models/test_job_model.py
+++ src/jobs/tests/test_models/test_job_model.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
-from jobs.models import Skill, Job
-from accounts.models import Employer
+from jobs.models import Skill, Job, Bid, RatingField
+from accounts.models import Employer, Freelancer
 from django.contrib.auth.models import User
 from django_countries import countries
 import tempfile
+from django.db.models import Avg
 
 
 class TestJobModel(TestCase):
@@ -23,7 +24,7 @@ class TestJobModel(TestCase):
             username='test_un'
         )
 
-        emp = Employer.objects.create(
+        self.emp = Employer.objects.create(
             profile_image=tempfile.NamedTemporaryFile(suffix=".jpg").name,
             mobile=1234567890,
             description='emp_desc',
@@ -33,7 +34,7 @@ class TestJobModel(TestCase):
         )
 
         self.job = Job.objects.create(
-            employer=emp,
+            employer=self.emp,
             title='title',
             description='job_desc',
             minimum_price=1,
@@ -41,6 +42,43 @@ class TestJobModel(TestCase):
         )
         self.job.skills_required.add(self.skill1)
         self.job.skills_required.add(self.skill2)
+
+        fr_user = User.objects.create(
+            first_name='test_fn_fr',
+            last_name='test_ln_fr',
+            username='test_un_fr'
+        )
+        self.freelancer = Freelancer.objects.create(
+            profile_image=tempfile.NamedTemporaryFile(suffix=".jpg").name,
+            mobile=1234567890,
+            description='fr_desc',
+            country=countries[0][0],
+            address='fr_address',
+            user=fr_user,
+        )
+
+        self.bid1 = Bid.objects.create(
+            job=self.job,
+            freelancer=self.freelancer,
+            amount=1000,
+            description='bid_desc1',
+            additional_description='addn_desc1',
+            comments='comment1',
+            accepted=False,
+            completed=False,
+            rating=RatingField().get_choices()[2][0]
+        )
+        self.bid2 = Bid.objects.create(
+            job=self.job,
+            freelancer=self.freelancer,
+            amount=12000,
+            description='bid_desc2',
+            additional_description='addn_desc2',
+            comments='comment2',
+            accepted=False,
+            completed=False,
+            rating=RatingField().get_choices()[3][0]
+        )
 
     def test_skill_str_func(self):
         self.assertEquals(self.job.__str__(), self.job.title)
@@ -52,3 +90,7 @@ class TestJobModel(TestCase):
     def test_job_skill_add(self):
         self.assertEquals(self.job.skills_required.all()[0], self.skill1)
         self.assertEquals(self.job.skills_required.all()[1], self.skill2)
+
+    def test_get_average_bid(self):
+        average_bid = Bid.objects.all().filter(job=self.job).aggregate(Avg('amount'))['amount__avg']
+        self.assertEquals(self.job.get_average_bid, average_bid)
