6179cb46f1fae15ed646bc59950a942491addc01
Chakshu Gupta <chakshugupta2000@gmail.com>
2019-03-22 18:22:55 +0530

    Create JobListView
    Created 'JobListView' in 'jobs/views.py' which uses template 'main/job_list.html' if user is authenticated otherwise redirects to template 'accounts/login.html'. Generic 'ListView' and 'LoginRequiredMixin' view used.

--- src/jobs/views.py
+++ src/jobs/views.py
@@ -1,3 +1,9 @@
-# from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Job
 
-# Create your views here.
+
+class JobListView(LoginRequiredMixin, ListView):
+    model = Job
+    context_object_name = 'job_list'
+    template_name = 'main/job_list.html'
