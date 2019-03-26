8be8bc79e23c70f26af992838cc4fdbb3d1c3649
Chakshu Gupta <chakshugupta2000@gmail.com>
2019-03-26 14:41:23 +0530

    Add filter in JobListView
    Added filter over list of jobs to be displayed in JobListView of 'jobs/views.py'. Freelancer user will see only those jobs that match his skill set and other will see none.

--- src/jobs/views.py
+++ src/jobs/views.py
@@ -1,9 +1,17 @@
 from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Job
+from accounts.models import Freelancer
 
 
 class JobListView(LoginRequiredMixin, ListView):
     model = Job
     context_object_name = 'job_list'
     template_name = 'main/job_list.html'
+
+    def get_queryset(self):
+        if hasattr(self.request.user, 'freelancer'):
+            freelancer_user_profile = Freelancer.objects.get(user=self.request.user)
+            return Job.objects.filter(skills_required__in=freelancer_user_profile.skills.all()).distinct()
+        else:
+            return None
