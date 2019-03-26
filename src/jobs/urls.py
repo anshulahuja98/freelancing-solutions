319f657632ee6362672da9916816f936e6be4e18
Chakshu Gupta <chakshugupta2000@gmail.com>
2019-03-26 14:38:03 +0530

    Add app_name and rename URL path name
    Added app_name 'jobs' in 'jobs/urls.py' and renamed path name from 'index' to 'job-list'.

--- src/jobs/urls.py
+++ src/jobs/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import JobListView
 
+app_name = 'jobs'
+
 urlpatterns = [
-    path('', JobListView.as_view(), name='index'),
+    path('', JobListView.as_view(), name='job-list'),
 ]
