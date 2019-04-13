074e135dea9bcdf5d866b336745076d145c109ab
Chakshu Gupta <chakshugupta2000@gmail.com>
2019-04-13 19:39:56 +0530

    Refractor Freelancer DashboardView
    Refractored 'DashboardView' of 'freelancer:urls.py' to 'FreelancerDashboardView'.

--- src/freelancer/urls.py
+++ src/freelancer/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path
-from freelancer.views import DashboardView
 from jobs.views import FreelancerBidListView
+from freelancer.views import FreelancerDashboardView
 
 app_name = 'freelancer'
 
 urlpatterns = [
-    path('dashboard/', DashboardView.as_view(), name='dashboard'),
-    path('bid-list', FreelancerBidListView.as_view(), name='bid-list')
+    path('bid-list', FreelancerBidListView.as_view(), name='bid-list'),
+    path('dashboard/', FreelancerDashboardView.as_view(), name='dashboard'),
 ]
