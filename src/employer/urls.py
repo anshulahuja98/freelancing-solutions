1e602c275672316d042581917217c4911b8c91b2
Anshul Ahuja <anshul.ahu@gmail.com>
2019-04-13 19:41:10 +0530

    Add employer Bid list

--- src/employer/urls.py
+++ src/employer/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from employer.views import DashboardView
-
+from jobs.views import EmployerBidListView
 
 app_name = 'employer'
 
 urlpatterns = [
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
+    path('bid-list', EmployerBidListView.as_view(), name='bid-list')
 ]
