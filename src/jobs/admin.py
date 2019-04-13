97d5d1b1d779b81361caa9686f3bfbc785a09b3b
Geetesh Gupta <gupta.15@iitj.ac.in>
2019-04-13 19:41:23 +0530

    Add docstrings in jobs app

--- src/jobs/admin.py
+++ src/jobs/admin.py
@@ -2,28 +2,25 @@ from django.contrib import admin
 from .models import Skill, Job, Bid  # Import Skill, Job, Bid module
 
 
-# Register Skill module on the admin interface
 @admin.register(Skill)
 class SkillAdmin(admin.ModelAdmin):
+    """Register Skill module on the admin interface"""
     class Meta:
         model = Skill
-        # All fields of Skill
         fields = '__all__'
 
 
-# Register Job module on the admin interface
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
+    """Register Job module on the admin interface"""
     class Meta:
         model = Job
-        # All fields of Job
         fields = '__all__'
 
 
-# Register Bid module on the admin interface
 @admin.register(Bid)
 class BidAdmin(admin.ModelAdmin):
+    """Register Bid module on the admin interface"""
     class Meta:
         model = Bid
-        # All fields of Bid
         fields = '__all__'
--- src/jobs/fields.py
+++ src/jobs/fields.py
@@ -1,21 +1,25 @@
 from django.db.models import fields
 
 
-# Custom RatingField inherits from inbuilt DecimalField of django
 class MoneyField(fields.DecimalField):
-    # Override constructor of DecimalField of django
-    def __init__(self, verbose_name=None, name=None, max_digits=8,
-                 decimal_places=2, **kwargs):  # Fix max digits to 8 and decimal places to 2
-        # Call constructor of the super class - DecimalField
+    """Custom MoneyField inherits from inbuilt DecimalField of django"""
+
+    def __init__(self, verbose_name=None, name=None, max_digits=8, decimal_places=2, **kwargs):
+        """Override constructor of DecimalField of django
+
+        Limit max digits to 8 and decimal places to 2
+        """
+
         super().__init__(verbose_name, name, **kwargs)
-        # Set default max digits and decimal_places
         self.max_digits, self.decimal_places = max_digits, decimal_places
 
 
-# Custom RatingField inherits from inbuilt IntegerField of django
 class RatingField(fields.IntegerField):
-    # Override constructor of IntegerField of django
+    """Custom RatingField inherits from inbuilt IntegerField of django"""
+
     def __init__(self, *args, **kwargs):
+        """Override constructor of IntegerField of django"""
+
         # Tuple of ratings presets
         RATINGS = (
             (0, 'NA'),
@@ -27,5 +31,4 @@ class RatingField(fields.IntegerField):
         )
         # Sends the RATINGS in the keyword arg of choices
         kwargs['choices'] = RATINGS
-        # Call constructor of the super class - IntegerField
         super(RatingField, self).__init__(*args, **kwargs)
