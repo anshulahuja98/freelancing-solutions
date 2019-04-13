6a4fbcc4ec9a3d8350a10cbd418d6f23c1cf740e
Anshul Ahuja <anshul.ahu@gmail.com>
2019-04-13 18:47:56 +0530

    Redirect authenticated user to respective dashboard

--- src/main/views.py
+++ src/main/views.py
@@ -1,6 +1,14 @@
 from django.views.generic import TemplateView
+from django.shortcuts import redirect, reverse
 
 
-# Create your views here.
 class IndexView(TemplateView):
     template_name = 'main/index.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if hasattr(request.user, 'employer'):
+            return redirect(reverse('employer:dashboard'))
+        elif hasattr(request.user, 'freelancer'):
+            return redirect(reverse('freelancer:dashboard'))
+        else:
+            return super().dispatch(request, *args, **kwargs)
