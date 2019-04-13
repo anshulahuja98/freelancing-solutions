839a71843c7268f110237aa09a35cf38fb50827c
Anshul Ahuja <anshul.ahu@gmail.com>
2019-04-13 19:41:38 +0530

    Add freelancer Bid list

--- src/freelancer/urls.py
+++ src/freelancer/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from freelancer.views import DashboardView
+from jobs.views import FreelancerBidListView
 
 app_name = 'freelancer'
 
 urlpatterns = [
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
+    path('bid-list', FreelancerBidListView.as_view(), name='bid-list')
 ]
