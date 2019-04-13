dd8c213b9df407af8944d8ffb95c232ca0287014
Anshul Ahuja <anshul.ahu@gmail.com>
2019-04-13 19:28:28 +0530

    Add Bid form for freelancer

--- src/jobs/forms.py
+++ src/jobs/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Job
+from .models import Job, Bid
 
 
 class JobForm(forms.ModelForm):
@@ -7,3 +7,9 @@ class JobForm(forms.ModelForm):
     class Meta:
         model = Job
         exclude = ('id',)
+
+
+class BidForm(forms.ModelForm):
+    class Meta:
+        model = Bid
+        fields = ['job', 'freelancer', 'amount', 'description', 'additional_description']
