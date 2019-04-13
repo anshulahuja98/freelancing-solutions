178d4a83c1dc95c7e8dd635cdd0cfa4ffaa0b41f
Chakshu Gupta <chakshugupta2000@gmail.com>
2019-04-13 19:36:29 +0530

    Refractor DashboardView
    Refractored 'DashboardView' of 'employer:urls.py' to 'EmployerDashboardView'

--- src/employer/urls.py
+++ src/employer/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from employer.views import DashboardView
 from jobs.views import EmployerBidListView
+from employer.views import EmployerDashboardView
 
 app_name = 'employer'
 
 urlpatterns = [
-    path('dashboard/', DashboardView.as_view(), name='dashboard'),
-    path('bid-list', EmployerBidListView.as_view(), name='bid-list')
+    path('bid-list', EmployerBidListView.as_view(), name='bid-list'),
+    path('dashboard/', EmployerDashboardView.as_view(), name='dashboard'),
 ]
