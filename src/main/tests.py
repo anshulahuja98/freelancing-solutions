d5b7df7872453042efbec8db4fcb8b1a7a88fc97
Geetesh Gupta <gupta.15@iitj.ac.in>
2019-04-14 16:17:36 +0530

    Add redirect tests in IndexViewTest

--- src/main/tests.py
+++ src/main/tests.py
@@ -1,9 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
+from django.contrib.auth.models import User
 from .views import IndexView
+from accounts.models import Freelancer, Employer
 
 
 class IndexViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.login_url = reverse('accounts:login')
+
+        self.test_user = User.objects.create_user(username='testuser', password='1X<ISRUkw+tuK')
+        self.client.login(username='testuser', password='1X<ISRUkw+tuK')
 
     def test_index_page_code(self):
         response = self.client.get('/')
@@ -21,3 +29,17 @@ class IndexViewTest(TestCase):
     def test_view_used(self):
         response = self.client.get(reverse('main:index'))
         self.assertEqual(response.resolver_match.func.__name__, IndexView.as_view().__name__)
+
+    def test_freelancer_login_index_redirect(self):
+        self.fr = Freelancer.objects.create(
+            user=self.test_user
+        )
+        response = self.client.get(reverse('main:index'), follow=True)
+        self.assertRedirects(response, reverse('freelancer:dashboard'))
+
+    def test_employer_login_index_redirect(self):
+        self.emp = Employer.objects.create(
+            user=self.test_user
+        )
+        response = self.client.get(reverse('main:index'), follow=True)
+        self.assertRedirects(response, reverse('employer:dashboard'))
